1b188566eeec1518729abc4a2c781c109c0a5dd6
Idris Chae <hokyung.chae@gmail.com>
2021-06-22 15:03:36 +0900

    detail page

--- frontend/user/src/pages/HistoryPage/HistoryDetailPage.jsx
+++ frontend/user/src/pages/HistoryPage/HistoryDetailPage.jsx
@@ -1,7 +1,16 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { useRouteMatch } from 'react-router-dom';
-import { Box, Text, Image, HStack } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Image,
+  HStack,
+  List,
+  ListItem,
+  Flex,
+  Badge,
+} from '@chakra-ui/react';
 
 export default function HistoryDetailPage() {
   const { url, params } = useRouteMatch();
@@ -13,6 +22,9 @@ export default function HistoryDetailPage() {
       .get(`/api/shoes/${params.historyID}`)
       .then(res => res.data);
   });
+  const queryClient = useQueryClient();
+  const userProfile = queryClient.getQueryData('history');
+  console.log(userProfile);
 
   return (
     <Box p={10}>
@@ -22,6 +34,44 @@ export default function HistoryDetailPage() {
           <HStack justify="center" p={4}>
             <Image maxW="400px" src={historyQuery.data?.imageUrl}></Image>
           </HStack>
+          <List>
+            <ListItem>
+              <Flex>
+                <Flex flex="1">사이즈</Flex>
+                <Flex flex="1">발매 수량</Flex>
+                <Flex flex="1">응모자 수</Flex>
+              </Flex>
+            </ListItem>
+            {Object.keys(historyQuery.data?.size).map(sz => (
+              <ListItem
+                key={sz}
+                fontSize={sz.split('_')[1] == userProfile.size ? '3xl' : 'l'}
+                bg={sz.split('_')[1] == userProfile.size ? 'teal' : 'default'}
+              >
+                <Flex>
+                  <Flex flex="1">{sz.split('_')[1]}</Flex>
+                  <Flex flex="1">{historyQuery.data?.size[sz].stocks}</Flex>
+                  <Flex flex="1">
+                    {historyQuery.data?.size[sz].applicants.length}
+                  </Flex>
+                </Flex>
+              </ListItem>
+            ))}
+          </List>
+          <Box>당첨여부:</Box>
+          <Box>
+            {historyQuery.data?.size[`m_${userProfile.size}`].winner?.findIndex(
+              v => v === userProfile._id
+            ) !== -1 ? (
+              <Badge fontSize="2xl" colorScheme="green">
+                당첨
+              </Badge>
+            ) : (
+              <Badge fontSize="2xl" colorScheme="red">
+                미당첨
+              </Badge>
+            )}
+          </Box>
         </Box>
       )}
     </Box>
